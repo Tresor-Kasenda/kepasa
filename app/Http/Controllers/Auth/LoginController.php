7349d98aee7515d9b80712caf7c528f1604415f0
scott tresor <scotttresor@gmail.com>
2022-03-01 10:57:36 +0100

    [FEAT] add redirect route trait

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,39 +5,13 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\Services\RedirectAuthentication;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    use AuthenticatesUsers;
-
-    protected string $redirectTo = RouteServiceProvider::HOME;
-
-    public function redirectTo(): string
-    {
-        switch(Auth::user()->role_id){
-            case 4:
-                $this->redirectTo = route('user.home.index');
-                return $this->redirectTo;
-                break;
-            case 3:
-                $this->redirectTo = route('organiser.organiser.index');
-                return $this->redirectTo;
-                break;
-            case 2:
-                $this->redirectTo = route('admin.admin.index');
-                return $this->redirectTo;
-                break;
-            case 1:
-                $this->redirectTo = route('supper.dashboard.index');
-                return $this->redirectTo;
-                break;
-            default:
-                $this->redirectTo = route('login');
-                return $this->redirectTo;
-        }
-    }
+    use AuthenticatesUsers, RedirectAuthentication;
 
     public function __construct()
     {
---
+++ app/Services/RedirectAuthentication.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Services;
+
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Auth;
+
+trait RedirectAuthentication
+{
+    protected string $redirectTo = RouteServiceProvider::HOME;
+
+    public function redirectTo(): string
+    {
+        switch(Auth::user()->role_id){
+            case 4:
+                $this->redirectTo = route('user.home.index');
+                return $this->redirectTo;
+                break;
+            case 3:
+                $this->redirectTo = route('organiser.organiser.index');
+                return $this->redirectTo;
+                break;
+            case 2:
+                $this->redirectTo = route('admin.admin.index');
+                return $this->redirectTo;
+                break;
+            case 1:
+                $this->redirectTo = route('supper.dashboard.index');
+                return $this->redirectTo;
+                break;
+            default:
+                $this->redirectTo = route('login');
+                return $this->redirectTo;
+        }
+    }
+}
