1454bc046ebf86748eaf42ca59491860af23beed
scott tresor <scotttresor@gmail.com>
2022-03-09 23:50:41 +0100

    [FIX] city Customer mail content

---
+++ app/Mail/CityCustomerMail.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class CityCustomerMail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @return void
+     */
+    public function __construct(public $promoted){}
+
+    public function build(): CityCustomerMail
+    {
+        return $this
+            ->to($this->promoted['email'])
+            ->subject('')
+            ->view('emails.contact', [
+                'contact' => $this->promoted
+            ]);
+    }
+}
