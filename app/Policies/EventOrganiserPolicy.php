ab295231209f5afd8ddcd7282f82532f60a8b391
Tresor-Kasenda <scotttresor@gmail.com>
2023-06-11 16:57:49 +0200

    wip organiser

---
+++ app/Policies/EventOrganiserPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Event;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class EventOrganiserPolicy
+{
+    use HandlesAuthorization;
+
+    public function update(User $user, Event $event): bool
+    {
+        return $user->id === $event->user_id;
+    }
+
+    public function delete(User $user, Event $event): bool
+    {
+        return $user->id === $event->user_id;
+    }
+}
---
+++ app/Repository/Organisers/Events/UpdateEventRepository.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Repository\Organisers\Events;
+
+
+use App\Http\Requests\Organiser\EventUpdateRequest;
+use App\Models\Billing;
+use App\Models\Category;
+use App\Models\City;
+use App\Models\Country;
+use App\Models\Event;
+use App\Services\EnableX\CreateRoomService;
+use App\Services\EnableX\EnableXHttpService;
+use App\Traits\FeedCalculation;
+use App\Traits\ImageUpload;
+use App\Traits\RandomValue;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+class UpdateEventRepository
+{
+    use EnableXHttpService;
+    use FeedCalculation;
+    use ImageUpload;
+    use RandomValue;
+
+
+    public function handle(EventUpdateRequest $request, Event $event): Event
+    {
+        $category = $this->getCategory(request: $request);
+
+        $this->updatedEvent(
+            $event,
+            $request,
+            $this->resolveFeeds(request: $request)
+        );
+
+        if (1 === $category->id) {
+            (new CreateRoomService())
+                ->update(
+                    request: $request,
+                    event: $event
+                );
+        }
+
+        $this->updatedBilling(
+            $event,
+            $request
+        );
+
+        return $event;
+    }
+
+    private function updatedEvent($event, $request, array $feedCalculation): void
+    {
+        $event->update([
+            'title' => $request->input('title'),
+            'subTitle' => $request->input('subTitle'),
+            'date' => $request->input('date'),
+            'startTime' => $request->input('startTime'),
+            'endTime' => $request->input('endTime'),
+            'address' => $request->input('address'),
+            'ticketNumber' => $request->input('ticketNumber'),
+            'prices' => $request->input('prices'),
+            'feeOption' => $request->input('feeOption'),
+            'commission' => $feedCalculation['0'],
+            'buyerPrice' => $feedCalculation['1'],
+            'country_id' => $this->getCountry($request)->id,
+            'city_id' => $this->getCity($request)->id,
+            'description' => $request->input('description'),
+            'category_id' => $request->input('category'),
+        ]);
+    }
+
+    private function getCity($request): Model|Builder|City|null
+    {
+        return City::query()
+            ->whereCityname($request->input('cityName'))
+            ->first();
+    }
+
+    private function getCountry($request): Builder|Model
+    {
+        return Country::query()
+            ->whereCountrycode( $request->input('country'))
+            ->first();
+    }
+
+    private function getCategory($request): null|Builder|Model
+    {
+        return Category::query()
+            ->where('id', '=', $request->input('category'))
+            ->first();
+    }
+
+    private function updatedBilling($event, $request): void
+    {
+        $amountSold = $request->input('ticketNumber') * $request->input('prices');
+        $commission = (5 / 100) * $amountSold;
+        $payout = $amountSold - $commission;
+        $billing = Billing::query()
+            ->where('event_id', '=', $event->id)
+            ->first();
+        $billing->update([
+            'eventDate' => $event->date,
+            'amountSold' => $amountSold,
+            'ticketPrice' => $event->prices,
+            'ticketSold' => $event->ticketNumber,
+            'commission' => $commission,
+            'feeType' => $request->input('feeOption'),
+            'amountPaid' => $payout,
+            'payout' => $payout,
+        ]);
+    }
+}
