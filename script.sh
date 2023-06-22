795e627e7ff1b5fb51454a85ca6faaf13be07174
Tresor-Kasenda <scotttresor@gmail.com>
2023-06-22 13:21:29 +0200

    first

---
+++ script.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+echo "What the name of project :"
+read name
+if [ $name ]; then
+    result = $(composer create-project --prefer-dist laravel/laravel $name)
+    echo $result
+    echo "Project $name has created with success"
+    cp $result $HOME/Vides
+    cd result
+    git init
+    git add .
+    echo "Add message of commit"
+    read message
+    if [ $message ]; then
+        git commit -m "$message"
+        git push origin main
+    fi
+fi
